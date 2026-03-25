f828a1ba4caf7bb1225c7cd88e31a9c6e49c75dc
Luizgabrielhd <lgmrttl@gmail.com>
2026-03-25 14:32:37 -0300

    02.js da lista de vetores

---
+++ lista-vetor-01/03.js
@@ -0,0 +1,9 @@
+let nums = [1,2,3,4,5];
+let soma =0;
+
+for(let i=0;i<nums.length;i++){
+    soma +=nums[i];
+
+}
+    console.log(soma);
+   
---
+++ lista-vetor-01/04.js
@@ -0,0 +1,10 @@
+let nums = [1,2,3,4,5];
+media =0;
+soma =0;
+
+for(let i=0;i<nums.length;i++){
+soma
+   media = nums[i]/2;
+
+}
+    console.log(media)
